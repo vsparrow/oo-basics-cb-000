8480e291f7c32182cbf2bc8764a4ff5c98f32052
Mohammad Khan <sparrowangels@gmail.com>
2017-09-07 19:00:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
+class Book
+  def initialize(title)
+    @title = title
+  end # initialize
+
+  def title
+    @title
+  end #title
+
+  def author=(author)
+    @author=author
+  end # author=
+
+  def author
+    @author
+  end # author
+
+  def page_count=(num)
+    @page_count=num
+  end #page_count=
+
+  def page_count
+    @page_count
+  end #page_count
 
 
+  def genre=(genre)
+    @genre=genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end #turn_page
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  def initialize(brand)
+    @brand=brand
+  end # initialize
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition="new"
+  end #cobble
+end #shoe
